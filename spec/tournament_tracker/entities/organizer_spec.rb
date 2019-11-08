96d5fa2955bdca8c98fba06946b53148d370d6c5
Rafael <RafaelB24@yahoo.com>
2019-11-08 09:54:27 -0800

    Add tests to check if Organizer can be initialized with attributes

--- spec/tournament_tracker/entities/organizer_spec.rb
+++ spec/tournament_tracker/entities/organizer_spec.rb
@@ -1,3 +1,16 @@
 RSpec.describe Organizer, type: :entity do
-  # place your tests here
+  it 'it can be initialized with username' do
+    organizer = Organizer.new(username: 'test')
+    expect(organizer.username).to eq('test') 
+  end 
+
+  it 'it can be initialized with password' do
+    organizer = Organizer.new(password_digest: 'test_password')
+    expect(organizer.password_digest).to eq('test_password') 
+  end 
+
+  it 'it can be initialized with email' do
+    organizer = Organizer.new(email: 'test@email.com')
+    expect(organizer.email).to eq('test@email.com') 
+  end 
 end
