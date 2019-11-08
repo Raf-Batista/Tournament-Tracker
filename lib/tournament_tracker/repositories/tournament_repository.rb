e20cf3a49137faea72d9ed9f3caac24e22a25e26
Rafael <RafaelB24@yahoo.com>
2019-11-08 12:06:57 -0800

    Add tests for tournament attributes and test if it has an organizer

--- lib/tournament_tracker/repositories/tournament_repository.rb
+++ lib/tournament_tracker/repositories/tournament_repository.rb
@@ -1,2 +1,9 @@
 class TournamentRepository < Hanami::Repository
+    associations do
+        belongs_to :organizer
+    end 
+
+    def find_with_organizer(id)
+        aggregate(:organizer).where(id: id).map_to(Organizer).one
+    end
 end
--- spec/tournament_tracker/entities/tournament_spec.rb
+++ spec/tournament_tracker/entities/tournament_spec.rb
@@ -1,3 +1,22 @@
 RSpec.describe Tournament, type: :entity do
-  # place your tests here
+  it 'can be initialized with name' do 
+    tournament = Tournament.new(name: 'test')
+    expect(tournament.name).to eq('test')
+  end 
+
+  it 'can be initialized with date' do 
+    date = Date.today
+    tournament = Tournament.new(date: date)
+    expect(tournament.date).to eq(date)
+  end 
+
+  it 'has an organizer' do 
+    organizer_repository = OrganizerRepository.new
+    organizer = organizer_repository.create(username: 'test_username', password_digest: 'test_password', email: 'test@email.com')
+    
+    tournament_repository = TournamentRepository.new
+    tournament = tournament_repository.create(name: 'test', date: Date.today, organizer_id: organizer.id)
+    
+    expect(tournament.id).to eq(organizer.id)
+  end 
 end
