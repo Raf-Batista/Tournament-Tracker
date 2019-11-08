88ca8fada2142689cb4df62fadfb21ed8f0371b9
Rafael <RafaelB24@yahoo.com>
2019-11-08 07:29:29 -0800

    Change adapter back to sql

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Hanami.configure do
     #    adapter :sql, 'postgresql://localhost/tournament_tracker_development'
     #    adapter :sql, 'mysql://localhost/tournament_tracker_development'
     #
-    adapter :pg, ENV.fetch('DATABASE_URL')
+    adapter :sql, ENV.fetch('DATABASE_URL')
 
     ##
     # Migrations
